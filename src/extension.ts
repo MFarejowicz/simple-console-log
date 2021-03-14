218185b5ec207f30d3f655aca646639bbfafbae1
Matthew Farejowicz <mfarejowicz@gmail.com>
2021-03-14 03:20:38 -0700

    some progress

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,22 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
 
+function getSpaces(currentLine: vscode.TextLine, document: vscode.TextDocument) {
+  if (currentLine.lineNumber === 0) {
+    return 0;
+  }
+
+  let previousLineNumber = currentLine.lineNumber - 1;
+  let previousLine = document.lineAt(previousLineNumber);
+
+  while (previousLineNumber > 0 && previousLine.isEmptyOrWhitespace) {
+    previousLineNumber -= 1;
+    previousLine = document.lineAt(previousLineNumber);
+  }
+
+  return previousLine.firstNonWhitespaceCharacterIndex;
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -18,9 +34,32 @@ export function activate(context: vscode.ExtensionContext) {
     // Display a message box to the user
     vscode.window.showInformationMessage("Hello VSCode from simple-console-log!");
   });
-
   context.subscriptions.push(disposable);
-}
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+  let logCommand = vscode.commands.registerCommand("simple-console-log.logHere", () => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      return;
+    }
+
+    const document = editor.document;
+    editor.edit((editBuilder) => {
+      const currPosition = editor.selection.active;
+      const currLine = document.lineAt(currPosition);
+      let insertPosition: vscode.Position;
+      let spaces: number;
+      if (currLine.isEmptyOrWhitespace) {
+        // insert in current line
+        insertPosition = currPosition.with({ character: 0 });
+        spaces = getSpaces(currLine, document);
+      } else {
+        // insert in next line
+        insertPosition = currPosition.translate(1).with({ character: 0 });
+        spaces = currLine.firstNonWhitespaceCharacterIndex;
+      }
+      editBuilder.insert(insertPosition, `${" ".repeat(spaces)}console.log("pog");\n`);
+    });
+    console.log(editor.selection.active);
+  });
+  context.subscriptions.push(logCommand);
+}
